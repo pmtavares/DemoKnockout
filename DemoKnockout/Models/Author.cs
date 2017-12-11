90fc1d3e4f77e6087148aad809cdf388b18a0881
pmtavares <pedrotavares41@gmail.com>
2017-12-11 21:42:29 +0000

    Create Authors Form Validation using knockout

--- DemoKnockout/Models/Author.cs
+++ DemoKnockout/Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,14 @@ namespace DemoKnockout.Models
     public class Author
     {
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+
         public string Biography { get; set; }
+
         public virtual ICollection<Book> Books { get; set; }
     }
 }
---
+++ DemoKnockout/Scripts/ViewModels/AuthorFormViewModel.js
@@ -0,0 +1,43 @@
+﻿function AuthorFormViewModel() {
+    var self = this;
+
+    self.saveCompleted = ko.observable(false);
+    self.sending = ko.observable(false);
+    
+    self.author = {
+        firstName: ko.observable(),
+        lastName: ko.observable(),
+        biography: ko.observable(),
+    };
+    
+    self.validateAndSave = function (form) {
+        if (!$(form).valid()) {
+            return false;
+        }
+
+        self.sending(true);
+        // include the anti forgery token
+        self.author.__RequestVerificationToken = form[0].value;
+        
+        $.ajax({
+            url: 'Create',
+            type: 'post',
+            contentType: 'application/x-www-form-urlencoded',
+            data: ko.toJS(self.author)
+        })
+            .success(self.successfulSave)
+            .error(self.errorSave)
+            .complete(function () { self.sending(false) });
+
+    };
+    
+    self.successfulSave = function () {
+        self.saveCompleted(true);
+        $('.body-content').prepend('<div class="alert alert-success"><strong> Success!</strong > The new author has been saved.</div > ');
+        setTimeout(function () { location.href = './'; }, 1000);
+    };
+
+    self.errorSave = function () {
+        $('.body-content').prepend('<div class="alert alert-danger"><strong> Error!</strong > There was an error creating the author.</div > ');
+    };
+}
